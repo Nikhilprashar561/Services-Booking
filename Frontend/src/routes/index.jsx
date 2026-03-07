83d400a54e7aa64cc7afd7b2a4c233e7cad0e3c5
nikhilprashar561 <nikhilprashar561@gmail.com>
2026-03-07 13:58:51 +0530

    everythink push

--- Frontend/src/routes/index.jsx
+++ Frontend/src/routes/index.jsx
@@ -1,4 +1,4 @@
-import App from "@/App";
+import App from "../App";
 import { createBrowserRouter } from "react-router-dom";
 import AdminProfilePage from "@/pages/AdminProfilePage";
 import AdminRegister from "@/pages/AdminRegister";
